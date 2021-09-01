7b980a6df774d429d149d1d3f9b71f99ae03b712
TheJaguarKing <eddie.diaz@me.com>
2021-08-31 22:10:46 -0500

    hw fin

--- index.js
+++ index.js
@@ -1,74 +1,220 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const path = require('path');
+const Manager = require("./lib/Manager");
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern");
 
 // create writeFile function using promises instead of a callback function
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // How do I make the prompt run indefinitely until the user wants me to stop?
 
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the employee name?',
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: 'What is the employee id?',
-    },
-    {
-      type: 'email',
-      name: 'id',
-      message: 'What is the employee email?',
-    },
-    {
-      type: 'list',
-      name: 'role',
-      message: 'What is employee role?',
-      choices: [Manager, Engineer, Intern];
-    },
+const team = [];
 
-// Is this syntax correct? How do I ask role-specific questions?
-// How do I connect my questions to my classes? How do I create new objects?
+const questionsAdd = [{
+  type: 'list',
+  name: 'choice',
+  message: 'Which employee do you wanna add?',
+  choices: ["Manager", "Engineer", "Intern", "No mOre"]
+}]
 
-  ]);
-};
+const questionIntern = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is the intern name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is the intern id?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is the intern email?',
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: 'What is the intern school?',
+  }
+]
 
-const generateHTML = (answers) =>
-  `<!DOCTYPE html>
+const questionManager = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is the Manager name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is the Manager id?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is the Manager email?',
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is the Manager officeNumber?',
+  }
+]
+
+const questionEngineer = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is the Engineer name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is the Engineer id?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is the Engineer email?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is the Engineer github?',
+  }
+]
+
+const createManager = () => {
+  //ask the question and get the answer
+  inquirer.prompt(questionManager).then((answers) => {
+    //create instance of Manager
+    const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    //add the Manager teams array
+    team.push(newManager)
+    addMember();
+  });
+}
+
+const createIntern = () => {
+  //ask the question and get the answer
+  inquirer.prompt(questionIntern).then((answers) => {
+    //create instance of Intern
+    const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    //add the Intern teams array
+    team.push(newIntern)
+    addMember();
+  });
+}
+
+const createEngineer = () => {
+  //ask the question and get the answer
+  inquirer.prompt(questionEngineer).then((answers) => {
+    //create instance of Engineer
+    const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    //add the Engineer teams array
+    team.push(newEngineer)
+    addMember();
+  });
+}
+
+const endGame = () => {
+  let template = "";
+
+  team.forEach((mem) => {
+    switch (mem.getRole()) {
+      case "Manager":
+        template+= `
+        <div class="container">
+          <h1 class="display-4">Manager name: ${mem.getName()}</h1>
+          <p class="lead">Role: ${mem.getRole()}.</p>
+          <p class="lead">Id: ${mem.getId()}.</p>
+          <p class="lead">Email: ${mem.getEmail()}.</p>
+          <p class="lead">OfficeNumber: ${mem.getOfficeNumber()}.</p>
+        </div>
+        `;
+        break;
+        case "Intern":
+          template+= `
+          <div class="container">
+            <h1 class="display-4">Intern name: ${mem.getName()}</h1>
+            <p class="lead">Role: ${mem.getRole()}.</p>
+            <p class="lead">Id: ${mem.getId()}.</p>
+            <p class="lead">Email: ${mem.getEmail()}.</p>
+            <p class="lead">School: ${mem.getSchool()}.</p>
+          </div>
+          `;
+          break;
+          case "Engineer":
+            template+= `
+            <div class="container">
+              <h1 class="display-4">Engineer name: ${mem.getName()}</h1>
+              <p class="lead">Role: ${mem.getRole()}.</p>
+              <p class="lead">Id: ${mem.getId()}.</p>
+              <p class="lead">Email: ${mem.getEmail()}.</p>
+              <p class="lead">github: ${mem.getGithub()}.</p>
+            </div>
+            `;
+            break;
+      default:
+        break;
+    }
+  });
+
+  const finalhtml = `
+  <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Employee name: ${answers.name}</h1>
-    <p class="lead">Role: ${answers.role}.</p>
-    <p class="lead">Id: ${answers.id}.</p>
-    <p class="lead">Email: ${answers.email}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-    </ul>
-  </div>
-</div>
+    ${template}
 </body>
-</html>`;
+</html>
+  `;
+
+
+  fs.writeFileAsync(path.join(__dirname,'/teams.html'), finalhtml)
+}
+
+const addMember = () => {
+  inquirer.prompt(questionsAdd).then((answer) => {
+    switch (answer.choice) {
+      case "Manager":
+        createManager();
+        break;
+      case "Intern":
+        createIntern();
+        break;
+      case "Engineer":
+        createEngineer();
+        break;
+      default:
+        endGame();
+        break;
+    }
+  });
+};
+
+
+addMember();
+
 
 // How can I make multiple containers for each employee?
 // How do I add employee specific information?
 
-const init = () => {
-  promptUser()
-    .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
-    .catch((err) => console.error(err));
-};
+// const init = () => {
+//   promptUser()
+//     .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+//     .then(() => console.log('Successfully wrote to index.html'))
+//     .catch((err) => console.error(err));
+// };
 
-init();
+// init();
--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,1 +1,10 @@
 // How do I write a test? I'm still lost on this.
+
+const { expect } = require("@jest/globals");
+const Employee = require("../lib/Employee")
+
+it("Testing Employee OBj", () => {
+    const mockEmployee = new Employee("dog","1","dog@dog.com");
+
+    expect(mockEmployee.getName()).toBe("dog");
+});
